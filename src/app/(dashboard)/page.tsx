9055aa3603e603132e1756c69c03e7cc435d3e32
Navneet Shahi <navneetshahi345@gmail.com>
2024-11-04 20:29:00 +0530

    "Added Collection model to Prisma schema and implemented CollectionList component in dashboard page"

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,4 +1,8 @@
+import CreateCollectionBtn from "@/components/CreateCollectionBtn";
+import Sadface from "@/components/icons/Sadface";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Skeleton } from "@/components/ui/skeleton";
+import prisma from "@/lib/prisma";
 import { wait } from "@/lib/wait";
 import { currentUser } from "@clerk/nextjs/server";
 import { Suspense } from "react";
@@ -8,6 +12,9 @@ export default async function Home() {
     <>
       <Suspense fallback={<WelcomeMsgFallback />}>
         <WelcomeMsg />
+      </Suspense>
+      <Suspense fallback={<div>Loading Collections...</div>}>
+        <CollectionList />
       </Suspense>
     </>
   );
@@ -19,7 +26,7 @@ async function WelcomeMsg() {
     return <div>error</div>;
   }
   return (
-    <div className="flex w-full">
+    <div className="flex w-full mb-12">
       <h1 className=" text-4xl font-bold">
         Welcome, <br /> {user?.firstName}
       </h1>
@@ -29,11 +36,33 @@ async function WelcomeMsg() {
 
 function WelcomeMsgFallback() {
   return (
-    <div className="flex w-full">
+    <div className="flex w-full mb-12">
       <h1 className=" text-4xl font-bold">
         <Skeleton className=" w-[150px] h-[36px]" />
         <Skeleton className=" w-[150px] h-[36px]" />
       </h1>
     </div>
   );
+}
+
+async function CollectionList() {
+  const user = await currentUser();
+  const collections = await prisma.collection.findMany({
+    where: {
+      userId: user?.id,
+    },
+  });
+
+  if (collections.length === 0) {
+    return (
+      <div className="flex flex-col gap-5">
+        <Alert>
+          <Sadface />
+          <AlertTitle>There are no collectins yet!</AlertTitle>
+          <AlertDescription>Create a collectin to get started</AlertDescription>
+        </Alert>
+        <CreateCollectionBtn />
+      </div>
+    );
+  }
 }
