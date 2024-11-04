d184b2327201e813ebe8d761167c58613f8b6bd3
Navneet Shahi <navneetshahi345@gmail.com>
2024-11-04 21:42:09 +0530

    "Added form functionality to CreateCollectionSheet component and updated tailwind.config.ts"

--- src/components/CreateCollectionSheet.tsx
+++ src/components/CreateCollectionSheet.tsx
@@ -1,11 +1,35 @@
+import { useForm } from "react-hook-form";
 import {
   Sheet,
   SheetContent,
   SheetDescription,
   SheetHeader,
   SheetTitle,
-  SheetTrigger,
 } from "./ui/sheet";
+import {
+  createCollectionSchema,
+  createCollectionSchemaType,
+} from "@/schema/createCollection";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+import { CollectionColor, CollectionColors } from "@/lib/constants";
+import { cn } from "@/lib/utils";
 
 interface Props {
   open: boolean;
@@ -13,6 +37,14 @@ interface Props {
 }
 
 function CreateCollectionSheet({ open, onOpenChange }: Props) {
+  const form = useForm<createCollectionSchemaType>({
+    defaultValues: {},
+    resolver: zodResolver(createCollectionSchema),
+  });
+
+  const onSubmit = (data: createCollectionSchemaType) => {
+    console.log("Submitted", data);
+  };
   return (
     <Sheet open={open} onOpenChange={onOpenChange}>
       <SheetContent>
@@ -22,6 +54,61 @@ function CreateCollectionSheet({ open, onOpenChange }: Props) {
             Collections are way to group your tasks.
           </SheetDescription>
         </SheetHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Personal" {...field} />
+                  </FormControl>
+                  <FormDescription>Collection Name</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="color"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Color</FormLabel>
+                  <FormControl>
+                    <Select>
+                      <SelectTrigger className="w-[180px]">
+                        <SelectValue
+                          placeholder="Color"
+                          className=" w-full h-8"
+                        />
+                      </SelectTrigger>
+                      <SelectContent className="w-full">
+                        {Object.keys(CollectionColors).map((color) => (
+                          <SelectItem
+                            key={color}
+                            value={color}
+                            className={cn(
+                              `w-full h-8 rounded-md my-1 text-white focus:text-white focus:font-bold focus:ring-2 ring-neutral-600 focus:ring-inset dark:focus:ring-white focus:px-8`,
+                              CollectionColors[color as CollectionColor]
+                            )}
+                          >
+                            {color}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                  </FormControl>
+                  <FormDescription>
+                    Select a color for your collection
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
       </SheetContent>
     </Sheet>
   );
